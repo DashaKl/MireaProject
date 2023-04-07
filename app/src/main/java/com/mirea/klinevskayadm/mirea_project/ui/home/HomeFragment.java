2ef332d43b98564962d778698c0fbe1c8e846c25
DashaKl <darya.klin444@gmail.com>
2023-04-07 11:48:31 +0300

    first

---
+++ app/src/main/java/com/mirea/klinevskayadm/mirea_project/ui/home/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.mirea.klinevskayadm.mirea_project.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.mirea.klinevskayadm.mirea_project.R;
+import com.mirea.klinevskayadm.mirea_project.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends Fragment {
+
+    private WebView webView;
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        webView = v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.kinopoisk.ru/");
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
